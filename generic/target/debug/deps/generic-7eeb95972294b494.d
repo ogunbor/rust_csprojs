72330475164b34fde9def1e5e2fe60c85ac25cd5
mompei <ogunborjerry@gmail.com>
2025-03-14 18:36:15 +0100

    generic example

---
+++ generic/target/debug/deps/generic-7eeb95972294b494.d
@@ -0,0 +1,5 @@
+/home/mompei/rust_projects/generic/target/debug/deps/libgeneric-7eeb95972294b494.rmeta: src/main.rs
+
+/home/mompei/rust_projects/generic/target/debug/deps/generic-7eeb95972294b494.d: src/main.rs
+
+src/main.rs:
---
+++ generic/target/debug/deps/generic-e0831c56db87ae11.d
@@ -0,0 +1,5 @@
+/home/mompei/rust_projects/generic/target/debug/deps/libgeneric-e0831c56db87ae11.rmeta: src/main.rs
+
+/home/mompei/rust_projects/generic/target/debug/deps/generic-e0831c56db87ae11.d: src/main.rs
+
+src/main.rs:
