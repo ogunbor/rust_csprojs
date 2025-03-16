8b7c1fbbe418b3851e8aec4105bb03e18cb97506
mompei <ogunborjerry@gmail.com>
2025-03-16 15:14:44 +0100

    reading positionals

---
+++ minigrep/target/debug/deps/minigrep-5ff4c25fe3a32d86.d
@@ -0,0 +1,5 @@
+/home/mompei/rust_projects/minigrep/target/debug/deps/libminigrep-5ff4c25fe3a32d86.rmeta: src/main.rs
+
+/home/mompei/rust_projects/minigrep/target/debug/deps/minigrep-5ff4c25fe3a32d86.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/deps/minigrep-75194d1d4c0f0cb7.d
@@ -0,0 +1,5 @@
+/home/mompei/rust_projects/minigrep/target/debug/deps/libminigrep-75194d1d4c0f0cb7.rmeta: src/main.rs
+
+/home/mompei/rust_projects/minigrep/target/debug/deps/minigrep-75194d1d4c0f0cb7.d: src/main.rs
+
+src/main.rs:
