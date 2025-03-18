586d022d48aacc76520130398ffd2c20e7202e7f
mompei <ogunborjerry@gmail.com>
2025-03-18 09:40:10 +0100

    Added testing

---
+++ minigrep/target/debug/deps/minigrep-3b0e5c5f26e495c8.d
@@ -0,0 +1,5 @@
+/home/mompei/rust_projects/minigrep/target/debug/deps/minigrep-3b0e5c5f26e495c8: src/main.rs
+
+/home/mompei/rust_projects/minigrep/target/debug/deps/minigrep-3b0e5c5f26e495c8.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/deps/minigrep-42f3ec11f5a3d9dd.d
@@ -0,0 +1,5 @@
+/home/mompei/rust_projects/minigrep/target/debug/deps/minigrep-42f3ec11f5a3d9dd: src/lib.rs
+
+/home/mompei/rust_projects/minigrep/target/debug/deps/minigrep-42f3ec11f5a3d9dd.d: src/lib.rs
+
+src/lib.rs:
